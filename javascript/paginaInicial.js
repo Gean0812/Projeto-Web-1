0166deee3723ae0e887c199278428bf2141f48a2
Gean Souza <51000198+Gean0812@users.noreply.github.com>
2019-11-06 15:27:11 -0300

    Agora tá mostrando os quadros

--- javascript/paginaInicial.js
+++ javascript/paginaInicial.js
@@ -13,7 +13,9 @@ class Quadro {
         div2.setAttribute("style","width: 15rem;");
 
         let spam = document.createElement("span");
-        spam.setAttribute("class", "border border-dark bg-warning");
+        spam.setAttribute("class", "border:none");
+        spam.style.backgroundColor=this.color;
+
 
         let texto = document.createTextNode(this.name);
 
@@ -35,6 +37,11 @@ var colorQ = document.getElementById("colorQuadro");
 var criarQuadro = document.getElementById("formQuadro");
 var listaDeQuadros = document.getElementById("listaDeQuadros");
 
+
+criarQuadro.addEventListener("onsubmit",function(e){
+    e.preventDefault();
+})
+
 listaDeQuadros.addEventListener("click",function(e){
     e.preventDefault();
    var cadastroQuadros = document.getElementById("cadastroQuadros").style.display="block";
@@ -47,6 +54,7 @@ criarQuadro.addEventListener("submit",function(e){
     e.preventDefault();
 
     novoQuadro();
+
 })
 
 console.log (sessionStorage.getItem("token"));
@@ -66,11 +74,12 @@ function novoQuadro () {
             var obj2 = JSON.parse(this.responseText);
             console.log(obj2);
             alert("Quadro criado");
+           
 
             let novoQuadro = new Quadro(obj2.name,obj2.color).iniciar();
             listaDeQuadros.appendChild(novoQuadro);
             
-            window.location = "../html/paginainicial.html";
+           // window.location = "../html/paginainicial.html";
         } else if (this.readyState == 4 && this.status == 400) {
             alert("Erro ao criar quadro");
         }
@@ -85,11 +94,22 @@ function novoQuadro () {
 
 function listarQuadros () {
 
+    var url3 = "https://tads-trello.herokuapp.com/api/trello/boards/:token";
+    var xhttp3 = new XMLHttpRequest();
+    xhttp3.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var obj3 = JSON.parse(this.responseText);
+            console.log(obj3);
 
+            return listaDeQuadros;
+
+        } else if (this.readyState == 4 && this.status == 400) {
+            alert("Erro");
+        }
+    }
+    xhttp3.open("POST", url3, true);
+    xhttp3.setRequestHeader("Content-type", "application/json");
+    xhttp3.send(JSON.stringify(dados2));
 
 
 }
-
-    
-
-
