d085d5953a81627a73e46efd25cc3e26048bbe60
Gean0812 <geanlucas0812@bol.com.br>
2019-11-08 22:06:56 -0300

    iniciando as listas

--- javascript/paginaInicial.js
+++ javascript/paginaInicial.js
@@ -1,40 +1,47 @@
 
 //CLASSE QUADRO
 class Quadro {
-    constructor(name,color,id){
+    constructor(name, color, id) {
         this.name = name;
         this.color = color;
-        this.id= id;
+        this.id = id;
     }
-    iniciar(){
+    iniciar() {
         let div = document.createElement("div");
-        div.setAttribute("class","container-fluid mh-100");
-        div.setAttribute("id"," ");
+        div.setAttribute("class", "container-fluid mh-100");
+        div.setAttribute("id", " ");
 
         let div2 = document.createElement("div2");
-        div2.setAttribute("class","card myCard");
-        div2.setAttribute("style","width: 15rem;");
-        div2.setAttribute("id",this.id);
+        div2.setAttribute("class", "card myCard");
+        div2.setAttribute("style", "width: 15rem;");
+        div2.setAttribute("id", this.id);
 
         let spam = document.createElement("span");
         spam.setAttribute("class", "border:none");
-        spam.style.backgroundColor=this.color;
+        spam.style.backgroundColor = this.color;
 
 
         let texto = document.createTextNode(this.name);
 
-        div.style.backgroundColor=this.color;
+        div.style.backgroundColor = this.color;
 
         spam.appendChild(texto);
         div2.appendChild(spam);
         div.appendChild(div2);
 
-        div.addEventListener("click", function(){
-           window.location= "quadros.html";
-           console.log("xd")
+        div.addEventListener("click",()=> {
+            let quadroInfo = {
+                "id": this.id,
+                "token": token,
+                "color":this.color,
+                "name":this.name
+            } 
+            sessionStorage.setItem("quadroInfo",JSON.stringify(quadroInfo));
+            window.location = "quadros.html";
+            console.log("xd")
         })
 
-        
+
 
         return div;
 
@@ -51,22 +58,28 @@ var quadrosApi;
 
 
 //EVENT LISTENNERS
-criarQuadro.addEventListener("onsubmit",function(e){
+criarQuadro.addEventListener("onsubmit", function (e) {
     e.preventDefault();
 })
 
-listaDeQuadros.addEventListener("click",function(e){
+listaDeQuadros.addEventListener("click", function (e) {
     e.preventDefault();
-   var cadastroQuadros = document.getElementById("cadastroQuadros").style.display="block";
+    var cadastroQuadros = document.getElementById("cadastroQuadros").style.display = "block";
 })
+
+// novaLista.addEventListener("onsubmit",function(e){
+//     e.preventDefault();
+
+//     criarList();
+// })
 
 
 //PEGANDO O TOKEN
 //SE ESTIVER COMO ESTÁ ABAIXO E DER ERRO NA FUNÇÃO QUE PASSA O TOKEN PODE SER PQ ESTÁ COM ASPAS,CASO ESTEJA CO MASPAS TEM QUE DAR O PARSE
-console.log (sessionStorage.getItem("token"));
+console.log(sessionStorage.getItem("token"));
 var token = JSON.parse(sessionStorage.getItem("token"));
 
-criarQuadro.addEventListener("submit",function(e){
+criarQuadro.addEventListener("submit", function (e) {
     e.preventDefault();
 
     novoQuadro();
@@ -79,7 +92,7 @@ criarQuadro.addEventListener("submit",function(e){
 //FUNÇÕES DA PÁGINA INICIAL
 
 //FUNÇÃO PARA CRIR O NOVO QUADRO
-function novoQuadro () {
+function novoQuadro() {
 
     var dados2 = {
         "name": document.getElementById("nomeQuadro").value,
@@ -94,12 +107,13 @@ function novoQuadro () {
             var obj2 = JSON.parse(this.responseText);
             console.log(obj2);
             alert("Quadro criado");
-           
 
-            let novoQuadro = new Quadro(obj2.name,obj2.color,obj2.id).iniciar();
+
+            let novoQuadro = new Quadro(obj2.name, obj2.color, obj2.id).iniciar();
+
             listaDeQuadros.appendChild(novoQuadro);
-            
-           // window.location = "../html/paginainicial.html";
+
+            // window.location = "../html/paginainicial.html";
         } else if (this.readyState == 4 && this.status == 400) {
             alert("Erro ao criar quadro");
         }
@@ -114,9 +128,9 @@ function novoQuadro () {
 
 
 //FUNÇÃO PARA LISTAR OS QUADROS
-function listarQuadros () {
+function listarQuadros() {
 
-    var url3 = "https://tads-trello.herokuapp.com/api/trello/boards/"+token ;
+    var url3 = "https://tads-trello.herokuapp.com/api/trello/boards/" + token;
     var xhttp3 = new XMLHttpRequest();
     xhttp3.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
@@ -125,9 +139,9 @@ function listarQuadros () {
 
             for (let index = 0; index < quadrosApi.length; index++) {
 
-                let novoQuadro = new Quadro(quadrosApi[index].name,quadrosApi[index].color,quadrosApi[index].id).iniciar();
+                let novoQuadro = new Quadro(quadrosApi[index].name, quadrosApi[index].color, quadrosApi[index].id).iniciar();
                 listaDeQuadros.appendChild(novoQuadro);
-                
+
             }
 
         } else if (this.readyState == 4 && this.status == 400) {
@@ -137,35 +151,6 @@ function listarQuadros () {
     xhttp3.open("GET", url3, true);
     xhttp3.setRequestHeader("Content-type", "application/json");
     xhttp3.send(JSON.stringify(url3));
-
-
-}
-
-
-//FUNÇÃO PARA RENOMEAR O QUADRO
-function renomearQuadro(){
-
-    var url4 = " https://tads-trello.herokuapp.com/api/trello/boards/rename" ;
-    var xhttp4 = new XMLHttpRequest();
-    xhttp4.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            quadrosApi = JSON.parse(this.responseText);
-            console.log(quadrosApi);
-
-            for (let index = 0; index < quadrosApi.length; index++) {
-
-                let novoQuadro = new Quadro(quadrosApi[index].name,quadrosApi[index].color,quadrosApi[index].id).iniciar();
-                listaDeQuadros.appendChild(novoQuadro);
-                
-            }
-
-        } else if (this.readyState == 4 && this.status == 400) {
-            alert("Erro");
-        }
-    }
-    xhttp4.open("GET", url4, true);
-    xhttp4.setRequestHeader("Content-type", "application/json");
-    xhttp4.send(JSON.stringify(url4));
 
 
 }
--- javascript/quadros.js
+++ javascript/quadros.js
@@ -1,35 +1,66 @@
+//CLASSE LISTA
+class Lista {
+    constructor(listName) {
+        this.listName = listName;
+    }
+}
 
 
+//VARIÁVEIS
+var novaLista = document.getElementById("novaLista");
+var formLista = document.getElementById("formNovaLista");
+var exibirForm = document.getElementById("criarListas");
+var btnCancelar = document.getElementById("cancelarLista");
+var listaDesc = document.getElementById("listaDesc");
+var btnEnviar = document.getElementById("enviarLista");
 
 
+//EVENT LISTENNERS
+novaLista.addEventListener("click",function(){
+    
+    exibirForm.style.display = "block"; 
+
+    
+})
+
+btnCancelar.addEventListener("click" ,function(e){
+    e.preventDefault();
+    exibirForm.style.display = "none";
+})
+
+btnEnviar.addEventListener("submit",function(e){
+    e.preventDefault();
+    criarList();
+})
 
 
+//FUNÇÃO PARA CRIAR UMA LISTA
+function criarList() {
+    var list = {
+        "name" : listaDesc.value,
+        "token" :sessionStorage.getItem("token"),
+        "board_id" : JSON.parse(sessionStorage.getItem("quadroInfo")).id
 
+    }
+    console.log(list);
 
-
-function renomearQuadro(){
-
-    var url4 = " https://tads-trello.herokuapp.com/api/trello/boards/rename" ;
+    var url4 = " https://tads-trello.herokuapp.com/api/trello/lists/new";
     var xhttp4 = new XMLHttpRequest();
     xhttp4.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            quadrosApi = JSON.parse(this.responseText);
-            console.log(quadrosApi);
+           var obj = JSON.parse(this.responseText);
+            console.log(obj);
+            alert("lista criada");
 
-            for (let index = 0; index < quadrosApi.length; index++) {
 
-                let novoQuadro = new Quadro(quadrosApi[index].name,quadrosApi[index].color,quadrosApi[index].id).iniciar();
-                listaDeQuadros.appendChild(novoQuadro);
-                
-            }
 
         } else if (this.readyState == 4 && this.status == 400) {
-            alert("Erro");
+            alert("Erro ao criar Lista");
         }
     }
-    xhttp4.open("GET", url4, true);
+    xhttp4.open("POST", url4, true);
     xhttp4.setRequestHeader("Content-type", "application/json");
-    xhttp4.send(JSON.stringify(url4));
+    xhttp4.send(JSON.stringify(list));
 
 
 }
